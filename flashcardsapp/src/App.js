9a2965f866f70df706a22eb598dba79347b25ed3
annaeliava <annaeliavad@gmail.com>
2022-10-18 01:15:55 +0300

    position

--- flashcardsapp/src/App.js
+++ flashcardsapp/src/App.js
@@ -1,12 +1,14 @@
-import styles from './assets/styles/app.scss';
-//import Header from './assets/components/common/header/Header';
-//import Footer from './assets/components/common/footer/Footer';
+import './assets/styles/app.scss';
+import Header from './assets/components/common/header/Header';
+import Footer from './assets/components/common/footer/Footer';
 import Dictionary from './assets/components/pages/dictionary/dictionary';
 
 function App() {
   return (
-    <div className={styles.App}>
+    <div className='App'>
+      <Header />
       <Dictionary />
+      <Footer />
     </div>
   );
 }
--- flashcardsapp/src/assets/components/common/header/header.jsx
+++ flashcardsapp/src/assets/components/common/header/header.jsx
@@ -5,13 +5,15 @@ import 'normalize.css';
 function Header() {
     return (
         <>
-            <div className={styles.container}>
-                <div className={styles.title}>IKNOWTHIS</div>
-                <div className={styles.currentLanguage}>EN</div>
-                <img src='/flashcardsapp/src/assets/pics/header/header_home-icon.svg' alt='Home Icon' className={styles.homeBtn} />
-                <img src="/flashcardsapp/src/assets/pics/common/userpic.svg" alt="Userpic" className={styles.myaccount} />
+            <div className={styles.mainContainer}>
+                <div className={styles.container}>
+                    <div className={styles.title}>IKNOWTHIS</div>
+                    <div className={styles.currentLanguage}>EN</div>
+                    <img src='/flashcardsapp/src/assets/pics/header/header_home-icon.svg' alt='Home Icon' className={styles.homeBtn} />
+                    <img src="/flashcardsapp/src/assets/pics/common/userpic.svg" alt="Userpic" className={styles.myaccount} />
+                </div>
+                <div className={styles.headerLine}></div>
             </div>
-            <div className={styles.headerLine}></div>
         </>
     );
 }
