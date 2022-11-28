7bdc1effb40b196cda40046e2444e9083bcf8de2
annaeliava <annaeliavad@gmail.com>
2022-11-28 16:42:09 +0300

    corrected styles & added newWordContainer

--- flashcardsapp/src/components/pages/dictionary/table/table.jsx
+++ flashcardsapp/src/components/pages/dictionary/table/table.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './Table.module.scss';
 import 'normalize.css';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Table(props) {
     const { data } = props;
@@ -15,13 +16,20 @@ export default function Table(props) {
                 </div>
                 <div className={styles.innerContainer}>
                     <div className={styles.tableNumber}>{data.number}</div>
-                    <div className={styles.tableWord}>{data.word}</div>
-                    <div className={styles.tableTranscription}>{data.transcription}</div>
-                    <div className={styles.tableTranslation}>{data.translation}</div>
+                    <input type='text' name='english' className={styles.tableWord} value={data.word} />
+                    <input type='text' name='transcription' className={styles.tableTranscription} value={data.transcription} />
+                    <input type='text' name='russian' className={styles.tableTranslation} value={data.translation} />
                     <button className={styles.deleteBtn}>X</button>
                 </div>
                 <div className={styles.btnContainer}>
-                    <button className={styles.addBtn}>+</button>
+                    <button className={styles.addBtn}><AddIcon /></button>
+                </div>
+                <div className={styles.newWordContainer} style={{ display: 'none' }}>
+                    <div className={styles.tableNumber}></div>
+                    <input type='text' name='english' className={styles.tableWord} />
+                    <input type='text' name='transcription' className={styles.tableTranscription} />
+                    <input type='text' name='russian' className={styles.tableTranslation} />
+                    <button className={styles.saveBtn}><AddIcon /></button>
                 </div>
             </div>
         </>
