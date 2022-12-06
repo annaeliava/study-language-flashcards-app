f2ea110af779fc72956d68eb925524c60542971b
annaeliava <annaeliavad@gmail.com>
2022-12-06 04:44:24 +0300

    corrections

--- flashcardsapp/src/components/pages/topics_words/food/Food.jsx
+++ flashcardsapp/src/components/pages/topics_words/food/Food.jsx
@@ -3,7 +3,7 @@ import Add from "../../../common/table/addBtn/addBtn";
 import Titles from "../../../common/table/firstLine/Titles";
 import Words from "../../../common/table/words/Words";
 import styles from "../Topics.module.scss";
-import food_data from "./food-topic_data"
+import food_data from "./food-topic_data";
 
 export default function Food() {
     return (
