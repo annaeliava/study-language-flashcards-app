82b27b9e14f782f8cd92d7da8e892e7ec0f202c7
annaeliava <annaeliavad@gmail.com>
2022-12-06 04:44:01 +0300

    added prevBtn, nextBtn and deleted two btns

--- flashcardsapp/src/components/pages/flashcards/Flashcard.jsx
+++ flashcardsapp/src/components/pages/flashcards/Flashcard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Card from './card/Card';
 import Answer from './answerBtn/Answerbtns.jsx';
 import styles from './Flashcard.module.scss';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const answerButtons_data = [
-    { background: 'E76B6B', icon: 'img/answerBtns/no.svg', width: '3.9375', height: '4.875' },
     { background: '6BB3E7', icon: 'img/answerBtns/dictionary.svg', width: '3.278125', height: '3.05125' },
-    { background: '92E76B', icon: 'img/answerBtns/yes.svg', width: '3.625', height: '3.1875' }
 ]
 
 const card_data = [
@@ -17,7 +17,9 @@ export default function Flashcards() {
     return (
         <>
             <div className={styles.container}>
+                <div className="prevBtn"><ArrowBackIcon /></div>
                 {card_data.map(card => <Card data={card} />)}
+                <div className="nextBtn"><ArrowForwardIcon /></div>
                 <div className={styles.buttonsContainer}>
                     {answerButtons_data.map(button => <Answer data={button} />)}
                 </div>
