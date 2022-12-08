e7f060ed9733bc268c42eb66e2a16ad62a4d5bc9
annaeliava <annaeliavad@gmail.com>
2022-12-09 00:31:57 +0300

    updated props

--- flashcardsapp/src/components/pages/flashcards/card/Card.jsx
+++ flashcardsapp/src/components/pages/flashcards/card/Card.jsx
@@ -1,28 +1,21 @@
 import React, { useState } from 'react';
 import styles from './Card.module.scss';
 
-Card.defaultProps = {
-    topic: '...',
-    word: 'Hello',
-    transcription: 'World',
-    translation: 'Sorry'
-};
-
 export default function Card(props) {
     const { data } = props;
     const [pressed, setPressed] = useState(false);
 
     const handleChange = () => {
         setPressed(!pressed);
-    }
+    };
     return (
         <>
             <div className={`${styles.container} ${pressed ? styles.showTranslation : ''}`} onClick={handleChange}>
                 <div className={styles.cardBody}>
                     <div className={styles.topic}>{data.topic}</div>
-                    <div className={styles.word}>{data.word}</div>
+                    <div className={styles.word}>{data.english}</div>
                     <div className={styles.transcription}>{data.transcription}</div>
-                    <div className={styles.translation}>{data.translation}</div>
+                    <div className={styles.translation}>{data.russian}</div>
                     <div className={styles.translations}>{data.additional}</div>
                 </div>
                 <div className={styles.numbers}>1/150</div>
@@ -30,3 +23,10 @@ export default function Card(props) {
         </>
     );
 }
+
+Card.defaultProps = {
+    topic: '...',
+    english: 'Hello',
+    transcription: 'World',
+    russian: 'Sorry'
+}; //почему не работает 
