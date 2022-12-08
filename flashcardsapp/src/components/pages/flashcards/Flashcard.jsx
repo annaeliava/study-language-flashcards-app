f6d7cb61f5567c322e03fe8bf3bbf120292b4a74
annaeliava <annaeliavad@gmail.com>
2022-12-09 00:32:36 +0300

    added new btns (prevBtn&nextBtn)

--- flashcardsapp/src/components/pages/flashcards/Flashcard.jsx
+++ flashcardsapp/src/components/pages/flashcards/Flashcard.jsx
@@ -7,22 +7,24 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const answerButtons_data = [
     { background: '6BB3E7', icon: 'img/answerBtns/dictionary.svg', width: '3.278125', height: '3.05125' },
-]
+];
 
 const card_data = [
-    { topic: 'basics', word: 'house', transcription: '/haʊs/', translation: 'дом', additional: 'здание, жилой дом' }
-]
+    { topic: 'basics', english: 'house', transcription: '/haʊs/', russian: 'дом', additional: 'здание, жилой дом' }
+];
 
 export default function Flashcards() {
     return (
         <>
             <div className={styles.container}>
-                <div className="prevBtn"><ArrowBackIcon /></div>
-                {card_data.map(card => <Card data={card} />)}
-                <div className="nextBtn"><ArrowForwardIcon /></div>
-                <div className={styles.buttonsContainer}>
-                    {answerButtons_data.map(button => <Answer data={button} />)}
+                <div className={styles.cardContainer}>
+                    <div className={styles.prevBtn}><ArrowBackIcon /></div>
+                    {card_data.map(card => <Card data={card} key={card.id} />)}
+                    <div className={styles.nextBtn}><ArrowForwardIcon /></div>
                 </div>
+                {/*<div className={styles.buttonsContainer}>
+                    {answerButtons_data.map(button => <Answer data={button} />)}
+    </div>*/}
             </div>
         </>
     );
