8f5ea9d3be408156ee27eff7526b0565c933f84d
annaeliava <annaeliavad@gmail.com>
2022-12-11 22:46:37 +0300

    updated data

--- flashcardsapp/src/components/pages/topics_words/verbs/verbs-topic_data.js
+++ flashcardsapp/src/components/pages/topics_words/verbs/verbs-topic_data.js
@@ -1,22 +1,20 @@
-import AddIcon from '@mui/icons-material/Add';
-
 const verbs_data = [
-    {id: 1, english:'to be', russian:'быть', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 2, english:'to do', russian:'делать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 3, english:'to have', russian:'иметь', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 4, english:'to come', russian:'приходить', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 5, english:'to go', russian:'идти', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 6, english:'to see', russian:'видеть', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 7, english:'to seem', russian:'казаться', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 8, english:'to give', russian:'дать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 9, english:'to take', russian:'брать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 10, english:'to keep', russian:'держать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 11, english:'to make', russian:'делать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 12, english:'to put', russian:'класть', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 13, english:'to send', russian:'посылать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 14, english:'to say', russian:'сказать', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 15, english:'to let', russian:'позволять', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
-    {id: 16, english:'to get', russian:'получить', transcription:'', topic:'verbs', deleteOrAddBtn: <AddIcon/>},
+    {id: 1, english:'to be', russian:'быть', transcription:'', topic:'verbs'},
+    {id: 2, english:'to do', russian:'делать', transcription:'', topic:'verbs'},
+    {id: 3, english:'to have', russian:'иметь', transcription:'', topic:'verbs'},
+    {id: 4, english:'to come', russian:'приходить', transcription:'', topic:'verbs'},
+    {id: 5, english:'to go', russian:'идти', transcription:'', topic:'verbs'},
+    {id: 6, english:'to see', russian:'видеть', transcription:'', topic:'verbs'},
+    {id: 7, english:'to seem', russian:'казаться', transcription:'', topic:'verbs'},
+    {id: 8, english:'to give', russian:'дать', transcription:'', topic:'verbs'},
+    {id: 9, english:'to take', russian:'брать', transcription:'', topic:'verbs'},
+    {id: 10, english:'to keep', russian:'держать', transcription:'', topic:'verbs'},
+    {id: 11, english:'to make', russian:'делать', transcription:'', topic:'verbs'},
+    {id: 12, english:'to put', russian:'класть', transcription:'', topic:'verbs'},
+    {id: 13, english:'to send', russian:'посылать', transcription:'', topic:'verbs'},
+    {id: 14, english:'to say', russian:'сказать', transcription:'', topic:'verbs'},
+    {id: 15, english:'to let', russian:'позволять', transcription:'', topic:'verbs'},
+    {id: 16, english:'to get', russian:'получить', transcription:'', topic:'verbs'},
 ];
 
 export default verbs_data;
