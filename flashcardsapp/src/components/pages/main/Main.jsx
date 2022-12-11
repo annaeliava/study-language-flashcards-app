71ba0442246b89accdc86544084ee1cb60d9b658
annaeliava <annaeliavad@gmail.com>
2022-12-11 23:04:25 +0300

    fixed main

--- flashcardsapp/src/components/pages/main/Main.jsx
+++ flashcardsapp/src/components/pages/main/Main.jsx
@@ -8,10 +8,10 @@ import food_data from '../topics_words/food/food-topic_data.js';
 import clothes_data from '../topics_words/clothes/clothes-topic_data.js';
 
 const topics_data = [
-    { topic: 'basic', quantity: basic_data.length + 1, linkToTopic: 'basic' },
-    { topic: 'verbs', quantity: verbs_data.length + 1, linkToTopic: 'verbs' },
-    { topic: 'food', quantity: food_data.length + 1, linkToTopic: 'food' },
-    { topic: 'clothes', quantity: clothes_data.length + 1, linkToTopic: 'clothes' }
+    { topic: 'basic', quantity: basic_data.length, linkToTopic: 'basic' },
+    { topic: 'verbs', quantity: verbs_data.length, linkToTopic: 'verbs' },
+    { topic: 'food', quantity: food_data.length, linkToTopic: 'food' },
+    { topic: 'clothes', quantity: clothes_data.length, linkToTopic: 'clothes' }
 ];
 
 const sidebar_data = [
