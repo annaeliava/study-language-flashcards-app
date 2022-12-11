2e9fee43cd6141bcffc0bdee4b60db4b13a831bb
annaeliava <annaeliavad@gmail.com>
2022-12-11 22:47:41 +0300

    added quantity

--- flashcardsapp/src/components/pages/main/Main.jsx
+++ flashcardsapp/src/components/pages/main/Main.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Topic from './topics/Topic.jsx';
 import Sidebar from './sidebar/Sidebar.jsx';
 import styles from './Main.module.scss';
+import basic_data from '../topics_words/basic/basic-topic_data.js';
+import verbs_data from '../topics_words/verbs/verbs-topic_data.js';
+import food_data from '../topics_words/food/food-topic_data.js';
+import clothes_data from '../topics_words/clothes/clothes-topic_data.js';
 
 const topics_data = [
-    { topic: 'basic', quantity: '41', linkToTopic: 'basic' },
-    { topic: 'verbs', quantity: '16', linkToTopic: 'verbs' },
-    { topic: 'food', quantity: '63', linkToTopic: 'food' },
-    { topic: 'clothes', quantity: '46', linkToTopic: 'clothes' }
+    { topic: 'basic', quantity: basic_data.length + 1, linkToTopic: 'basic' },
+    { topic: 'verbs', quantity: verbs_data.length + 1, linkToTopic: 'verbs' },
+    { topic: 'food', quantity: food_data.length + 1, linkToTopic: 'food' },
+    { topic: 'clothes', quantity: clothes_data.length + 1, linkToTopic: 'clothes' }
 ];
 
 const sidebar_data = [
