17d83a5d1fc5f9543655a1bf294b2c85eaa8173d
annaeliava <annaeliavad@gmail.com>
2022-12-17 02:34:56 +0300

    added useRef

--- flashcardsapp/src/components/common/table/newWord/New.jsx
+++ flashcardsapp/src/components/common/table/newWord/New.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "../Table.module.scss";
 import DoneIcon from '@mui/icons-material/Done';
 
 export default function New(props) {
     const { saveNewWord } = props;
 
+    const refCard = useRef();
+
+    useEffect(() => refCard.current.focus(), []);
+
     return (
         <>
             <div className={styles.newWordContainer}>
                 <div className={styles.tableNumber}>*</div>
-                <input type='text' name='english' className={styles.tableWord} />
+                <input type='text' name='english' className={styles.tableWord} ref={refCard} />
                 <input type='text' name='transcription' className={styles.tableTranscription} />
                 <input type='text' name='russian' className={styles.tableTranslation} />
                 <button className={styles.saveBtn} onClick={saveNewWord}><DoneIcon /></button>
